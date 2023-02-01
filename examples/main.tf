d8df39704648672e1183eb20ec6fb4f2bf679d5a
Omer Brumer <brumeromer@gmail.com>
2023-02-01 19:21:50 +0200

    Changes

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "aks_private_dns_zone" {
-  source = "git::https://gitlab.com/OmerBrumer/private_dns_zone.git"
+  source = "git::https://github.com/OmerBrumer/module-private-dns-zone.git"
 
   private_dns_zone_name = "brumerfinalterraformworkspoke.private.westeurope.azmk8s.io"
   resource_group_name   = "brumer-final-terraform-workspoke-rg"
